7a2e8f7e72da3a8298c57c6b015f473d2faad2ee
nancyalgazzar <94644017+nancyalgazzar@users.noreply.github.com>
2022-01-07 20:27:23 +0200

    Update SwitchToDesignMode.cpp

--- SwitchToDesignMode.cpp
+++ SwitchToDesignMode.cpp
@@ -2,6 +2,9 @@
 #include "Input.h"
 #include "Output.h"
 #include "Grid.h"
+#include "CardEleven.h"
+#include "CardNine.h"
+#include "CardTen.h"
 SwitchToDesignMode::SwitchToDesignMode(ApplicationManager* pApp) :Action(pApp)
 {
 	// The constructor initializes the ApplicationManager pointer data member
@@ -21,5 +24,19 @@ void SwitchToDesignMode::Execute()
 {
 	Grid* pGrid = pManager->GetGrid();
 	Output* pOut=pGrid->GetOutput();
+	pOut->ClearStatusBar();
 	pOut->CreateDesignModeToolBar();
+	CardTen::Pplayer1 = NULL;
+	CardEleven::Pplayer1 = NULL;
+	CardNine::Pplayer1 = NULL;
+	
+	CellPosition StartCell(1);
+	for (int i = 0; i < 4; i++)
+	{
+		Player* player = pGrid->GetCurrentPlayer();
+		pGrid->UpdatePlayerCell(player, StartCell);
+		player->SetturnCount(0);
+		pGrid->AdvanceCurrentPlayer();
+
+	}
 }
