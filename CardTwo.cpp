7459a48e31cec10913206a11403f08242ddedf32
YaraHisham61 <88517271+YaraHisham61@users.noreply.github.com>
2022-01-08 01:58:54 +0200

    Update CardTwo.cpp

--- CardTwo.cpp
+++ CardTwo.cpp
@@ -12,14 +12,11 @@ void CardTwo::ReadCardParameters(Grid* pGrid)
 		This card's class has no data members
 		so we don't need any input from the user
 	*/
-	Output* pOut = pGrid->GetOutput();
-	pOut->PrintMessage("New CardTwo: You will be moved to the next ladder (if found) ");  //Printing to a descriptive message to the user
-
-	pOut->ClearStatusBar();  //Clearing the status bar
 }
 
 void CardTwo::Apply(Grid* pGrid, Player* pPlayer)
 {
+	pPlayer = pGrid->GetCurrentPlayer();
 	Ladder* pLadder= pGrid->GetNextLadder(position);   //Searching for the next ladder postion in the grid
 	if (pLadder == NULL)   
 	{
@@ -31,8 +28,16 @@ void CardTwo::Apply(Grid* pGrid, Player* pPlayer)
 	{
 		Card::Apply(pGrid, pPlayer);  //Calling Apply() of the base class Card to print a message that you reached this card number
 		CellPosition NewPos = pLadder->GetPosition();  //Setting the new postion of the player to be the postion of the next ladder
-		pGrid->UpdatePlayerCell(pPlayer, NewPos);    //Moving the player to the new postion (start cell of the next ladder) 
+		pGrid->UpdatePlayerCell(pPlayer, NewPos); //Moving the player to the new postion (start cell of the next ladder) 
+
+		GameObject* pGameObject = pPlayer->GetCell()->GetGameObject();  //Checking if the new cell has a game object to apply?
+		if (pGameObject)
+		{
+
+			pGameObject->Apply(pGrid, pPlayer);
+		}
 	}
+
 }
 
 CardTwo::~CardTwo()
