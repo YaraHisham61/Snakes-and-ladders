cdcb5e8782869b533e96cd1c7af960ac5fa381c5
YaraHisham61 <88517271+YaraHisham61@users.noreply.github.com>
2022-01-08 01:59:28 +0200

    Update CardThree.cpp

--- CardThree.cpp
+++ CardThree.cpp
@@ -11,17 +11,11 @@ void CardThree::ReadCardParameters(Grid* pGrid)
 		This card's class has no data members
 		so we don't need any input from the user
 	*/
-
-	Output* pOut = pGrid->GetOutput();
-	pOut->PrintMessage("New CardThree: You will get an extra dice roll");   //Printing to a descriptive message to the user
-	pOut->ClearStatusBar();   //Clearing the status bar 
 }
 
 void CardThree::Apply(Grid* pGrid, Player* pPlayer)
 {
 	Card::Apply(pGrid, pPlayer);   //Calling Apply() of the base class Card to print a message that you reached this card number
-	
-
 	for (int i = 0;i < MaxPlayerCount - 1;i++)
 	{
 		pGrid->AdvanceCurrentPlayer();  //Giving the player extra dice roll by increasing players count
