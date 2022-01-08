228c917b59bcf9ee37b4ff9401441bf3da3cccb6
YaraHisham61 <88517271+YaraHisham61@users.noreply.github.com>
2022-01-08 02:18:20 +0200

    Update CardFour.cpp

--- CardFour.cpp
+++ CardFour.cpp
@@ -2,7 +2,8 @@
 
 CardFour::CardFour(const CellPosition& pos) :Card(pos)  // set the cell position of the card
 {
-	cardNumber = 4;   // set the inherited cardNumber data member with the card number (4 here)
+	cardNumber = 4;  // set the inherited cardNumber data member with the card number (4 here)
+
 }
 
 void CardFour::ReadCardParameters(Grid* pGrid)
@@ -11,16 +12,13 @@ void CardFour::ReadCardParameters(Grid* pGrid)
 		This card's class has no data members
 		so we don't need any input from the user
 	*/
-
-	Output* pOut = pGrid->GetOutput();
-	pOut->PrintMessage("New CardFour: You will be prevented frrom rolling the dice the next turn");  //Printing to a descriptive message to the user
-	pOut->ClearStatusBar();   //Clearing the status bar 
 }
 
 void CardFour::Apply(Grid* pGrid, Player* pPlayer)
 {
 	Card::Apply(pGrid, pPlayer);  //Calling Apply() of the base class Card to print a message that you reached this card number
-	//Not done yet
+	pPlayer = pGrid->GetCurrentPlayer();
+	pPlayer->setgame(1); //preventing the player from playing
 }
 
 CardFour::~CardFour()
