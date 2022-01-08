f98345fbe0d67b445eb633158e6eb6ecd8a75f58
YaraHisham61 <88517271+YaraHisham61@users.noreply.github.com>
2022-01-08 02:19:31 +0200

    Update CardFive.cpp

--- CardFive.cpp
+++ CardFive.cpp
@@ -1,4 +1,3 @@
-
 #include "CardFive.h"
 
 CardFive::CardFive(const CellPosition& pos) : Card(pos)   // set the cell position of the card
@@ -12,10 +11,6 @@ void CardFive::ReadCardParameters(Grid* pGrid)
 		This card's class has no data members
 		so we don't need any input from the user
 	*/
-
-		Output * pOut = pGrid->GetOutput();
-	pOut->PrintMessage("New CardFive: You will be moved backward with the same number of steps that you just rolled ");   //Printing to a descriptive message to the user
-	pOut->ClearStatusBar();   //Clearing the status bar 
 }
 
 void CardFive::Apply(Grid* pGrid, Player* pPlayer)
@@ -25,6 +20,12 @@ void CardFive::Apply(Grid* pGrid, Player* pPlayer)
 	int tocell = position.GetCellNumFromPosition(position)-pPlayer->getjustRolledDiceNum();  //Calculating the new cellnumber of the player
 	CellPosition topostion = position.GetCellPositionFromNum(tocell);  //getting the postion of the new cell number
 	pGrid->UpdatePlayerCell(pPlayer, topostion); //moving the player to the new postion
+
+	GameObject* pGameObject = pPlayer->GetCell()->GetGameObject();  //Checking if the new cell has a game object to apply?
+	if (pGameObject)
+	{
+		pGameObject->Apply(pGrid, pPlayer);
+	}
 }
 
 CardFive::~CardFive()
